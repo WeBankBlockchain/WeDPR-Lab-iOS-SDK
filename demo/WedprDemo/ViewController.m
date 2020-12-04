770012882b7987db3376b3c9bd30b1f8b358c850
qyan-dev <qyan_dev@163.com>
2020-12-04 11:17:28 +0800

    v1.2 clean part 2

--- demo/WedprDemo/ViewController.m
+++ demo/WedprDemo/ViewController.m
@@ -19,14 +19,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Test your demo code here.
-    vclDemo(2,2,4);
     cryptoDemo();
-    selectiveDisclosureDemo();
+    vclDemo(2,2,4);
+    scdDemo();
 
 }
 
+void cryptoDemo() {
+    printf("\n*******\nCRYPTO DEMO RUN\n*******\n");
+    NSError *error;
+    // Generates a keyPair from the secp256k1 curve.
+    NSString *keyPairStr = @(*wedpr_secp256k1_gen_key_pair());
+    if(keyPairStr.length == 0) {
+        // The above API call should not fail.
+        printf("API loading error");
+        return;
+    }
+    NSData *decodedData = [[NSData alloc] initWithBase64EncodedString:keyPairStr options:0];
+    Keypair *keyPair = [Keypair parseFromData:decodedData error:&error];
+    printf("public key = :%s", keyPair.publicKey);
+    printf("private key = :%s", keyPair.privateKey);
+
+    // Base64 encoding for "WeDPR Demo", which is currently required to pass bytes input to API.
+    // TODO: Allow non-encoded UTF8 input.
+    NSString *message = @"V2VEUFIgRGVtbw==";
+
+    // Hashes a message with keccak256 hash.
+    NSString *messageHash = @(*wedpr_keccak256_hash([message UTF8String]));
+    printf("messageHash = :%s", messageHash);
+
+    // Signs this message hash with ECDSA on the secp256k1 curve.
+    NSString *signature = @(*wedpr_secp256k1_sign([keyPair.privateKey UTF8String], [messageHash UTF8String]));
+    printf("signature = :%s", signature);
+
+    // Verifies this signature for the above message hash.
+    int result = wedpr_secp256k1_verify([keyPair.publicKey UTF8String], [messageHash UTF8String], [signature UTF8String]);
+    printf("signature verify result = :%s", result);
+
+    // Encrypts the message with ECIES on the secp256k1 curve.
+    NSString *encryptedData = @(*wedpr_secp256k1_ecies_encrypt([keyPair.publicKey UTF8String], [message UTF8String]));
+    printf("encryptedData = :%s", encryptedData);
+
+    // Decrypts the message with ECIES on the secp256k1 curve.
+    NSString *decryptedData = @(*wedpr_secp256k1_ecies_decrypt([keyPair.privateKey UTF8String], [encryptedData UTF8String]));
+    printf("decryptedData = :%s", decryptedData);
+}
+
 void vclDemo(unsigned long(c1Value), unsigned long(c2Value), unsigned long(c3Value)) {
-    printf("\n*******\nVCL PROOF RUN\n*******\n");
+    printf("\n*******\nVCL DEMO RUN\n*******\n");
     NSError *error;
 
     // Makes a confidential credit record and owner secret for c1Value.
@@ -82,143 +122,119 @@ void vclDemo(unsigned long(c1Value), unsigned long(c2Value), unsigned long(c3Val
     result = wedpr_vcl_verify_range([c1Result.credit UTF8String], [rangeProofStr UTF8String]);
 }
 
-void cryptoDemo() {
-    printf("\n*******\nCRYPTO TOOL RUN\n*******\n");
+void scdDemo() {
+    printf("\n*******\nSCD DEMO RUN\n*******\n");
     NSError *error;
-    // Generates a keyPair from the secp256k1 curve.
-    NSString *keyPairStr = @(*wedpr_secp256k1_gen_key_pair());
-    if(keyPairStr.length == 0) {
+
+    // An issuer defines the certificate schema and generates the certificate template.
+    schema *schema = [[schema alloc] init];
+    [schema.attributeNameArray addObject:@"name"];
+    [schema.attributeNameArray addObject:@"age"];
+    [schema.attributeNameArray addObject:@"gender"];
+    [schema.attributeNameArray addObject:@"issue_time"];
+    NSString *schemaStr = [[schema data] base64EncodedStringWithOptions:0];
+
+    NSString *scdResultStr = @(*wedpr_scd_make_certificate_template([schemaStr UTF8String]));
+    if(scdResultStr.length == 0) {
         // The above API call should not fail.
         printf("API loading error");
         return;
     }
-    NSData *decodedData = [[NSData alloc] initWithBase64EncodedString:keyPairStr options:0];
-    Keypair *keyPair = [Keypair parseFromData:decodedData error:&error];
-    printf("public key = :%s", keyPair.publicKey);
-    printf("private key = :%s", keyPair.privateKey);
+    // TODO: Find a better way to do ScdResult decoding.
+    NSData *decodedData = [[NSData alloc] initWithBase64EncodedString:scdResultStr options:0];
+    ScdResult *scdResult = [ScdResult parseFromData:decodedData error:&error];
+    printf("certificateTemplate = :%s", scdResult.certificateTemplate);
+    printf("templatePrivateKey = :%s", scdResult.templatePrivateKey);
+    NSString *templatePrivateKeyStr = [[scdResult.templatePrivateKey data] base64EncodedStringWithOptions:0];
+    NSString *certificateTemplateStr = [[scdResult.certificateTemplate data] base64EncodedStringWithOptions:0];
 
-    // Base64 encoding for "WeDPR Demo", which is currently required to pass bytes input to API.
-    // TODO: Allow non-encoded UTF8 input.
-    NSString *message = @"V2VEUFIgRGVtbw==";
-
-    // Hashes a message with keccak256 hash.
-    NSString *messageHash = @(*wedpr_keccak256_hash([message UTF8String]));
-    printf("messageHash = :%s", messageHash);
-
-    // Signs this message hash with ECDSA on the secp256k1 curve.
-    NSString *signature = @(*wedpr_secp256k1_sign([keyPair.privateKey UTF8String], [messageHash UTF8String]));
-    printf("signature = :%s", signature);
-
-    // Verifies this signature for the above message hash.
-    int result = wedpr_secp256k1_verify([keyPair.publicKey UTF8String], [messageHash UTF8String], [signature UTF8String]);
-    printf("signature verify result = :%s", result);
-
-    // Encrypts the message with ECIES on the secp256k1 curve.
-    NSString *encryptedData = @(*wedpr_secp256k1_ecies_encrypt([keyPair.publicKey UTF8String], [message UTF8String]));
-    printf("encryptedData = :%s", encryptedData);
-
-    // Decrypts the message with ECIES on the secp256k1 curve.
-    NSString *decryptedData = @(*wedpr_secp256k1_ecies_decrypt([keyPair.privateKey UTF8String], [encryptedData UTF8String]));
-    printf("decryptedData = :%s", decryptedData);
-}
-
-void selectiveDisclosureDemo() {
-    printf("\n*******\nSELECTIVE DISCLOSURE RUN\n*******\n");
-    // issuer make template
-
-    NSError *error;
-    CertificateSchema *certificateSchema = [[CertificateSchema alloc] init];
-    [certificateSchema.attributeNameArray addObject:@"age"];
-    [certificateSchema.attributeNameArray addObject:@"id"];
-    [certificateSchema.attributeNameArray addObject:@"time"];
-    
-    
-    NSString *certificateSchemaStr = [[certificateSchema data] base64EncodedStringWithOptions:0];
-    
-    NSString *selectiveDisclosureResultStr = @(*wedpr_scd_make_certificate_template([certificateSchemaStr UTF8String]));
-    if(selectiveDisclosureResultStr.length == 0) {
-        // The above API call should not fail.
-        printf("API loading error");
-        return;
-    }
-    NSData *decodedData = [[NSData alloc] initWithBase64EncodedString:selectiveDisclosureResultStr options:0];
-    ScdResult *selectiveDisclosureResult = [ScdResult parseFromData:decodedData error:&error];
-    printf("certificateTemplate = :%s", selectiveDisclosureResult.certificateTemplate);
-    printf("templatePrivateKey = :%s", selectiveDisclosureResult.templatePrivateKey);
-    NSString *templatePrivateKeyStr = [[selectiveDisclosureResult.templatePrivateKey data] base64EncodedStringWithOptions:0];
-    NSString *certificateTemplateStr = [[selectiveDisclosureResult.certificateTemplate data] base64EncodedStringWithOptions:0];
-    
-    // User fill template
-    AttributeDict *attributeDict = [[AttributeDict alloc] init];
+    // A user fills the certificate template and prepares a request for the issuer to sign.
+    AttributeDict *certificateData = [[AttributeDict alloc] init];
     StringToStringPair *stringToStringPair = [[StringToStringPair alloc] init];
+    // TODO: Add a utility function to convert any string to a decimal string.
+    // Before this utility function is implemented, the attribute value can only be a decimal
+    // string.
+    stringToStringPair.key = @"name";
+    stringToStringPair.value = @"123";
+    [certificateData.pairArray addObject:stringToStringPair];
     stringToStringPair.key = @"age";
-    stringToStringPair.key = @"18";
-    [attributeDict.pairArray addObject:stringToStringPair];
-    stringToStringPair.key = @"id";
-    stringToStringPair.key = @"123456";
-    [attributeDict.pairArray addObject:stringToStringPair];
-    stringToStringPair.key = @"time";
-    stringToStringPair.key = @"20201124";
-    [attributeDict.pairArray addObject:stringToStringPair];
-    
-    NSString *attributeDictStr = [[attributeDict data] base64EncodedStringWithOptions:0];
+    stringToStringPair.value = @"19";
+    [certificateData.pairArray addObject:stringToStringPair];
+    stringToStringPair.key = @"gender";
+    stringToStringPair.value = @"1";
+    [certificateData.pairArray addObject:stringToStringPair];
+    stringToStringPair.key = @"issue_time";
+    stringToStringPair.value = @"12345";
+    [certificateData.pairArray addObject:stringToStringPair];
+    NSString *certificateDataStr = [[certificateData data] base64EncodedStringWithOptions:0];
 
-    selectiveDisclosureResultStr = @(*wedpr_scd_fill_certificate([attributeDictStr UTF8String], [certificateTemplateStr UTF8String]));
-    printf("signCertificateRequest = :%s", selectiveDisclosureResult.signCertificateRequest);
-    printf("userPrivateKey = :%s", selectiveDisclosureResult.userPrivateKey);
-    printf("certificateSecretsBlindingFactors = :%s", selectiveDisclosureResult.certificateSecretsBlindingFactors);
-    printf("userNonce = :%s", selectiveDisclosureResult.userNonce);
-    
-    NSString *certificateSecretsBlindingFactors = selectiveDisclosureResult.certificateSecretsBlindingFactors;
-    NSString *userPrivateKey = selectiveDisclosureResult.userPrivateKey;
-    NSString *userNonce = selectiveDisclosureResult.userNonce;
+    scdResultStr = @(*wedpr_scd_fill_certificate([certificateDataStr UTF8String], [certificateTemplateStr UTF8String]));
+    decodedData = [[NSData alloc] initWithBase64EncodedString:scdResultStr options:0];
+    scdResult = [ScdResult parseFromData:decodedData error:&error];
+    printf("signCertificateRequest = :%s", scdResult.signCertificateRequest);
+    printf("userPrivateKey = :%s", scdResult.userPrivateKey);
+    printf("certificateSecretsBlindingFactors = :%s", scdResult.certificateSecretsBlindingFactors);
+    printf("userNonce = :%s", scdResult.userNonce);
 
-    
-    // Issuer sign user's request to generate credential
-    NSString *signCertificateRequestStr = [[selectiveDisclosureResult.signCertificateRequest data] base64EncodedStringWithOptions:0];
-    
-    selectiveDisclosureResultStr = @(*wedpr_scd_sign_certificate([certificateTemplateStr UTF8String], [templatePrivateKeyStr UTF8String], [signCertificateRequestStr UTF8String], @"123456", [userNonce UTF8String]));
-    
-    printf("certificateSignature = :%s", selectiveDisclosureResult.certificateSignature);
-    printf("issuerNonce = :%s", selectiveDisclosureResult.issuerNonce);
-    NSString *issuerNonce = selectiveDisclosureResult.issuerNonce;
-    
-    // User generate new credentialSignature
-    NSString *certificateSignatureStr = [[selectiveDisclosureResult.certificateSignature data] base64EncodedStringWithOptions:0];
-    
-    selectiveDisclosureResultStr = @(*wedpr_scd_blind_certificate_signature([certificateSignatureStr UTF8String], [attributeDictStr UTF8String], [certificateTemplateStr UTF8String], [userPrivateKey UTF8String], [certificateSecretsBlindingFactors UTF8String], [issuerNonce UTF8String]));
-    
-    printf("New certificateSignature = :%s", selectiveDisclosureResult.certificateSignature);
-    NSString *newCertificateSignatureStr = [[selectiveDisclosureResult.certificateSignature data] base64EncodedStringWithOptions:0];
-    
+    NSString *certificateSecretsBlindingFactors = scdResult.certificateSecretsBlindingFactors;
+    NSString *userPrivateKey = scdResult.userPrivateKey;
+    NSString *userNonce = scdResult.userNonce;
 
-    // Verifier set verification rules
+    // The issuer verifies the certificate signing request from the user and signs the certificate.
+    NSString *signCertificateRequestStr = [[scdResult.signCertificateRequest data] base64EncodedStringWithOptions:0];
+
+    scdResultStr = @(*wedpr_scd_sign_certificate([certificateTemplateStr UTF8String], [templatePrivateKeyStr UTF8String], [signCertificateRequestStr UTF8String], @"123456", [userNonce UTF8String]));
+    decodedData = [[NSData alloc] initWithBase64EncodedString:scdResultStr options:0];
+    scdResult = [ScdResult parseFromData:decodedData error:&error];
+    printf("certificateSignature = :%s", scdResult.certificateSignature);
+    printf("issuerNonce = :%s", scdResult.issuerNonce);
+    NSString *issuerNonce = scdResult.issuerNonce;
+
+    // The user blinds the received certificateSignature to prevent the issuer to track the
+    // certificate usage.
+    NSString *certificateSignatureStr = [[scdResult.certificateSignature data] base64EncodedStringWithOptions:0];
+
+    scdResultStr = @(*wedpr_scd_blind_certificate_signature([certificateSignatureStr UTF8String], [certificateDataStr UTF8String], [certificateTemplateStr UTF8String], [userPrivateKey UTF8String], [certificateSecretsBlindingFactors UTF8String], [issuerNonce UTF8String]));
+    decodedData = [[NSData alloc] initWithBase64EncodedString:scdResultStr options:0];
+    scdResult = [ScdResult parseFromData:decodedData error:&error];
+    printf("New certificateSignature = :%s", scdResult.certificateSignature);
+
+    NSString *newCertificateSignatureStr = [[scdResult.certificateSignature data] base64EncodedStringWithOptions:0];
+
+    // A verifier sets a verification rule to:
+    // Check AGE > 18 and,
     VerificationRuleSet *verificationRuleSet = [[VerificationRuleSet alloc] init];
     Predicate *predicate = [[Predicate alloc] init];
     predicate.attributeName = @"age";
     predicate.predicateType = @"GT";
-    predicate.predicateValue = 17;
+    predicate.predicateValue = 18;
     [verificationRuleSet.attributePredicateArray addObject:predicate];
-    predicate.attributeName = @"gender";
-    predicate.predicateType = @"EQ";
-    predicate.predicateValue = 1;
-    [verificationRuleSet.attributePredicateArray addObject:predicate];
-    
-    NSString *verificationRuleStr = [[verificationRuleSet data] base64EncodedStringWithOptions:0];
-    
-    selectiveDisclosureResult = @(*wedpr_scd_get_verification_nonce());
-    NSString *verificationNonce = selectiveDisclosureResult.verificationNonce;
-    
-    selectiveDisclosureResult = @(*wedpr_scd_prove_selective_disclosure([verificationRuleStr UTF8String], [newCertificateSignatureStr UTF8String], [attributeDictStr UTF8String], [certificateTemplateStr UTF8String], [userPrivateKey UTF8String], [verificationNonce UTF8String]));
-    
-    printf("verifyRequest = :%s", selectiveDisclosureResult.verifyRequest);
-    
-    NSString *verifyRequestStr = [[selectiveDisclosureResult.verifyRequest data] base64EncodedStringWithOptions:0];
-    
-    selectiveDisclosureResult = @(*wedpr_scd_verify_selective_disclosure([verificationRuleStr UTF8String], [verifyRequestStr UTF8String]));
-    printf("boolResult = :%s", selectiveDisclosureResult.boolResult);
-    
-    selectiveDisclosureResult = @(*wedpr_scd_get_revealed_attributes([verifyRequestStr UTF8String]));
-    printf("revealedAttributeDict = :%s", selectiveDisclosureResult.revealedAttributeDict);
+    // Reveal the issue_time attribute.
+    [verificationRuleSet.revealedAttributeNameArray addObject:@"issue_time"];
+    NSString *verificationRuleSetStr = [[verificationRuleSet data] base64EncodedStringWithOptions:0];
+
+    scdResultStr = @(*wedpr_scd_get_verification_nonce());
+    decodedData = [[NSData alloc] initWithBase64EncodedString:scdResultStr options:0];
+    scdResult = [ScdResult parseFromData:decodedData error:&error];
+    NSString *verificationNonce = scdResult.verificationNonce;
+
+    // The user proves the signed certificate data satisfying the verification rules and does not
+    // reveal any extra data.
+    scdResultStr = @(*wedpr_scd_prove_selective_disclosure([verificationRuleSetStr UTF8String], [newCertificateSignatureStr UTF8String], [certificateDataStr UTF8String], [certificateTemplateStr UTF8String], [userPrivateKey UTF8String], [verificationNonce UTF8String]));
+    decodedData = [[NSData alloc] initWithBase64EncodedString:scdResultStr options:0];
+    scdResult = [ScdResult parseFromData:decodedData error:&error];
+    printf("verifyRequest = :%s", scdResult.verifyRequest);
+
+    NSString *verifyRequestStr = [[scdResult.verifyRequest data] base64EncodedStringWithOptions:0];
+
+    scdResultStr = @(*wedpr_scd_verify_selective_disclosure([verificationRuleSetStr UTF8String], [verifyRequestStr UTF8String]));
+    decodedData = [[NSData alloc] initWithBase64EncodedString:scdResultStr options:0];
+    scdResult = [ScdResult parseFromData:decodedData error:&error];
+    printf("boolResult = :%s", scdResult.boolResult);
+
+    scdResult = @(*wedpr_scd_get_revealed_attributes([verifyRequestStr UTF8String]));
+    decodedData = [[NSData alloc] initWithBase64EncodedString:scdResultStr options:0];
+    scdResult = [ScdResult parseFromData:decodedData error:&error];
+    printf("revealedCertificateData = :%s", scdResult.revealedAttributeDict);
 }
 @end
