b3735ad1c31cdf84f4f7249ee357b5f0ed70b33d
HaoXuan40404 <444649358@qq.com>
2021-03-25 11:37:48 +0800

    add ktb demo

---
+++ demo/WedprDemo/Wedpr/WedprKtb.h
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/**
+ * C interface for 'wedpr_hdk_create_master_key_en'.
+ */
+char *wedpr_hdk_create_master_key_en(char *password_cstring, char *mnemonic_cstring);
+
+/**
+ * C interface for 'wedpr_hdk_create_mnemonic_en'.
+ */
+char *wedpr_hdk_create_mnemonic_en(unsigned char word_count);
+
+/**
+ * C interface for 'wedpr_hdk_derive_extended_key'.
+ */
+char *wedpr_hdk_derive_extended_key(char *master_key_cstring,
+                                    int purpose_type,
+                                    int asset_type,
+                                    int account,
+                                    int change,
+                                    int address_index);
